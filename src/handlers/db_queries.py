ae1648cd7ec5ab9e80d72485c4b6e93cb672a13f
Mauricio <zelaya.mauricio@gmail.com>
2020-06-10 18:40:41 +0000

    initial navigation plus first db actions

---
+++ src/handlers/db_queries.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Control123!'
+app.config['MYSQL_DB'] = 'agenda'
+mysql = MySQL(app)
+
